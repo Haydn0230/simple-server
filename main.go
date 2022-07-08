10df4159c433a80a8e3dc58d1e1ce38d93ce61cd
Haydn Brierley-Jones <haydn.brierley-jones@skybettingandgaming.com>
2022-07-08 12:36:50 +0100

    initial commit

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println(r)
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, "Hello!")
+	})
+
+	fmt.Printf("Starting server at port 8080\n")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+}
